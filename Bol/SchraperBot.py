30e09f3249f736735d949c73141b6665f11d2973
Kikkerslijm410 <Lucasclavel356@gmail.com>
2023-04-09 18:13:47 +0200

    bol works

--- Bol/SchraperBot.py
+++ Bol/SchraperBot.py
@@ -6,13 +6,13 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-with open('Bol_com_reviews.csv', 'w', newline='') as csvfile:
-    fieldnames = ['name', 'price', 'url']
+with open('Bol_com_reviews.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['name', 'review']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
     # loop through all pages
-    for page in range(1,5):
+    for page in range(1,50):
         url = f'https://www.bol.com/nl/nl/l/rugzakken/20701/?page={page}'
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -20,9 +20,16 @@ with open('Bol_com_reviews.csv', 'w', newline='') as csvfile:
         product_tags = soup.find_all('li', {'class': 'product-item--column'})
 
         for tag in product_tags:
+            # Scrape de naam, prijs en url van het product
             name = tag.find('span', {'class': 'truncate'}).text.strip()
-            price = soup.find('meta', {'itemprop': 'price'})['content']
             url = tag.find('a', {'class': 'hit-area-listpage'})['href']
+            
+            # Ga naar de website om te kijken of er reviews zijn
             visit_url = f'https://www.bol.com{url}'
-            print(name)
-            writer.writerow({'name': name, 'price': price, 'url': url})
+            response = requests.get(visit_url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            review_tags = soup.find_all('div', {'class': 'review__body'})
+
+            for review_tag in review_tags:
+                review = review_tag.text.strip()
+                writer.writerow({'name': name, 'review': review})

7caf14a8f5eb0e1bcfb3814f0e8a4968d8c76b6e
Kikkerslijm410 <lucasclavel356@gmail.com>
2023-04-09 16:22:51 +0200

    done

---
+++ SchraperApi.py
@@ -0,0 +1,72 @@
+from csv import writer
+import requests
+import json
+
+outfile = "data"
+storageList = []
+
+#======File managment=======
+def BeverAPIWriteDataToJsonFile():
+    with open(outfile+".json", 'w', encoding="utf8", newline="") as out:
+        json.dump(storageList, out) 
+        
+def BeverAPIWriteDataToCSVFile():
+    with open(outfile+".csv", 'w', encoding="utf8", newline="") as out:
+        write = writer(out)
+        write.writerow(["sku","rating","good points","bad points"])
+    with open(outfile+".csv", 'a', encoding="utf8", newline="") as out:
+        write = writer(out)
+        for entry in storageList:
+            write.writerow(entry)
+            
+            
+#======Helpers==============
+
+def appendToList(list, data):
+    if(not data == None):
+        list.append(data)
+        
+def BeverGetSKUNr(url:str):
+    return url.split("-")[-1].split(".")[0]
+
+#=====Main methods========
+def BeverGetReviewsFromURL(url:str):
+   skuNr = BeverGetSKUNr(url)
+   request = requests.get("https://widgets.reevoo.com/api/product_reviews?per_page=3&trkref=BEV&sku="+skuNr+"&locale=nl-NL&display_mode=embedded&page=1")
+   if(request.ok):
+    data = json.loads(request.content)
+
+    nrOfPages = int(data["body"]["pagination"]["total_pages"])
+    
+    for i in range(nrOfPages):
+        data = BeverGetReviewFromURL(skuNr, i+1, outfile)
+        if not data == None and not data in storageList:
+            storageList.append(data)
+         
+def BeverGetReviewFromURL(skuNr, pagenr:int, outfile):
+    request = requests.get("https://widgets.reevoo.com/api/product_reviews?per_page=3&trkref=BEV&sku="+skuNr+"&locale=nl-NL&display_mode=embedded&page="+str(pagenr))
+    data = json.loads(request.content)
+    data = data["body"]["reviews"]
+    
+    rating = []
+    goodPoints = []
+    badpoints = []
+    
+    for thingy in data:
+        try:
+            appendToList(rating, thingy["overall_score"])
+        except:
+            None
+        try:
+            appendToList(goodPoints, thingy["text"]["good_points"])
+        except:
+            None
+        try:
+            appendToList(badpoints, thingy["text"]["bad_points"])
+        except:
+            None
+    if len(goodPoints) > 0 or len(badpoints > 0):
+        return [skuNr,rating,goodPoints,badpoints]
+    return None
+    
+# BeverGetReviewsFromURL("https://www.bever.nl/p/ayacucho-annapurna-softshell-B12AD90130.html?colour=4168")
---
+++ SchraperBot.py
@@ -0,0 +1,140 @@
+import csv
+from threading import Thread
+import time
+from csv import writer
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options 
+from selenium.webdriver.common.by import By
+import SchraperApi as BeverAPI
+
+url = "https://www.bever.nl/c/uitrusting/rugzakken/wandelrugzakken.html"
+outputfile = 'Beverbot.csv'
+products = []
+productNrs = []
+
+def getBrowser():
+    ffoptions = Options()
+    # ffoptions.add_argument("--headless")
+    return webdriver.Firefox(options=ffoptions)    
+
+def BeverInitialSetup(browser:webdriver.Firefox):
+    browser.get(url)
+    browser.find_element(By.ID, 'accept-all-cookies').click()
+    element = browser.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[2]/div/div/div/div[8]/div/div/a[3]/span")
+    pageAmount = element.get_attribute("textContent")
+    print(pageAmount)
+    return int(pageAmount)
+
+def BeverLoadFindAllURLs(browser:webdriver.Firefox, baseurl):
+    pageAmount = BeverInitialSetup(browser)
+    url = baseurl
+    links = []
+    for i in range(pageAmount):
+        url = baseurl+"?page="+str(i)
+        links.extend(BeverGetURLFromPage(browser, url))
+    return links
+        
+def BeverGetURLFromPage(browser:webdriver.Firefox, page):
+    browser.get(page)
+    URLS = []
+    Products = browser.find_elements(By.XPATH, "//a[@class = 'as-a-link as-a-link--container as-m-product-tile__link']")
+    
+    for thing in Products:
+        print(thing.get_attribute("href"))
+
+    for element in Products:
+        link = element.get_attribute("href")
+        if(not "https://www.bever.nl" in link):
+            URLS.append('https://www.bever.nl' + link)
+        else:
+            URLS.append(link)
+    return URLS
+
+def BeverGetProductData(browser:webdriver.Firefox, url):
+    SkuNr = BeverAPI.BeverGetSKUNr(url)
+    if(not SkuNr in productNrs):
+
+        try:
+            browser.get(url)
+            try:
+                browser.find_element(By.ID, 'accept-all-cookies').click()
+            except:
+                None
+            #Get info
+            
+            brand = browser.find_element(By.XPATH, "//a[@class = 'as-a-link as-a-link--base']").text
+            product = browser.find_element(By.XPATH, "//span[@class = 'as-a-text as-a-text--title']").text
+            price = browser.find_element(By.XPATH, "//span[@data-qa = 'sell_price']").text.replace('€', '').replace(",",".")
+            
+            info = [SkuNr,brand, product, price]
+            products.append(info)
+            productNrs.append(SkuNr)
+            BeverAPI.BeverGetReviewsFromURL(url)
+        except:
+            None
+
+#=====================File Management==============================================
+def writeToOutput(item):
+    with open(outputfile, 'a', encoding="utf8", newline="") as out:
+        write = writer(out)
+        write.writerow(item)
+
+def stringInOutput(item):
+    with open(outputfile, 'r', encoding="utf8", newline="") as out:
+        items = csv.reader(out, delimiter=",")
+        for line in items:
+            if item[1] in line[1] and item[2] == line[2]:
+                print("IM WORKING!")
+                return True
+    return False
+
+def setupOutputFile():
+    with open(outputfile, 'w', encoding="utf8", newline="") as out:
+        out.write("")
+
+#=====================Threading====================================================
+class MyThread(Thread):
+    def __init__(self, name):
+        """Initialize the thread"""
+        Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+        """Run the thread"""
+        threadName = self.name
+        threadNumber = int(threadName.split('#')[1]) #Convert the string containing the Thread name to a int for use in lists
+        browser = getBrowser()
+        while len(links) > 0:
+            link = links.pop()
+            BeverGetProductData(browser, link)
+            print(threadName +" Finished "+ BeverAPI.BeverGetSKUNr(link))
+        browser.close()
+
+def create_threads():
+    """
+    Create a group of threads
+    """
+    for i in range(int(len(links)/50)): #Spawns a thread for each entry in a list threads (len(urls))
+        name = "Thread #%s" % (i)
+        my_thread = MyThread(name)
+        my_thread.start()
+    my_thread.join() 
+    time.sleep(5)
+    
+    writeToOutput(["sku","brand","product","price"])
+    for product in products:
+        writeToOutput(product)
+        
+    BeverAPI.BeverAPIWriteDataToJsonFile()
+    BeverAPI.BeverAPIWriteDataToCSVFile()
+    print(len(productNrs))
+
+if __name__ == "__main__":
+    browser = getBrowser()
+    global links
+    setupOutputFile()
+    links = BeverLoadFindAllURLs(browser, url)
+    print(len(links))
+    browser.close()
+    create_threads()
+    

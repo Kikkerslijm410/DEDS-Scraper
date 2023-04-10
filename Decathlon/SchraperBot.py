7ed7c48538ee40248cacb5744b3efca42480979a
Kikkerslijm410 <lucasclavel356@gmail.com>
2023-04-10 11:38:50 +0200

    hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm

--- Decathlon/SchraperBot.py
+++ Decathlon/SchraperBot.py
@@ -6,13 +6,12 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-with open('Decathlon_com_reviews.csv', 'w', newline='') as csvfile:
+with open('Decathlon_com_reviews.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['name', 'price', 'url', 'review']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-    # loop through all pages
-    for page in range(1,8):
+    for page in range(1, 8):
         x = page * 32
         url = f'https://www.decathlon.nl/browse/c0-sporten/c1-wandelen/c2-rugzakken/_/N-25efpl?from={x}&size=32'
         response = requests.get(url, headers=headers)
@@ -22,17 +21,30 @@ with open('Decathlon_com_reviews.csv', 'w', newline='') as csvfile:
 
         for tag in product_tags:
             name = tag.find('h2', {'class': 'vtmn-p-0'}).text.strip()
-            price_tag = soup.find('span', {'class': 'vtmn-price'})
-            price = price_tag.text.strip()            
+            # price_tag = soup.find('span', {'class': 'vtmn-price'})
+            # price = price_tag.text.strip()
             url = tag.find('a', {'class': 'dpb-product-model-link'})['href']
-            writer.writerow({'name': name, 'price': price, 'url': url})
-            
-            # Ga naar de website om te kijken of er reviews zijn
-            visit_url = f'https://www.decathlon.nl{url}'
-            response = requests.get(visit_url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            review_tags = soup.find_all('div', {'class': 'review__body'})
 
-            for review_tag in review_tags:
-                review = review_tag.text.strip()
-                writer.writerow({'name': name, 'review': review})
+            visit_url = f'https://www.decathlon.nl{url}'
+            response = requests.get(visit_url, headers=headers)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            review_link = soup.find('a', text='Toon alle reviews')
+            print (review_link)
+            # review_link = soup2.find('a', {'class': 'cta'})
+            # print (review_link)
+            # url2 = tag.find('a', {'class': 'cta cta--alt'})['href']
+            # print(url2)
+            
+            
+            
+            # print (review_link)
+            # if review_link is not None:
+            #     url2 = review_link['href']
+            #     visit_url2 = f'https://www.decathlon.nl{url2}'
+            #     response = requests.get(visit_url2, headers=headers)
+            #     soup3 = BeautifulSoup(response.content, 'html.parser')
+            #     # review_tags = soup3.find_all('p', {'class': 'answer-body'})
+            #     # for review_tag in review_tags:
+            #     #     review = review_tag.text.strip()
+            #     writer.writerow({'name': name, 'review': url2})
+

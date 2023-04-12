48b57e76238349e5d368c38bdbda252a0a066e08
Kikkerslijm410 <lucasclavel356@gmail.com>
2023-04-12 13:17:19 +0200

    images are downloaded

--- Bol/SchraperBot.py
+++ Bol/SchraperBot.py
@@ -6,15 +6,13 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-with open('Bol_com_reviews2.csv', 'w', newline='', encoding='utf-8') as csvfile:
+with open('Bol_com_reviews2.csv', 'w', newline='', encoding='utf-16') as csvfile:
     fieldnames = ['name', 'review', 'image']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-    # loop through all pages (page (2)6 is fucked) 
-    # for page in range (1,6);
-    for page in range(7,25):
-        print ('huidige pagina: ' + str(page))
+    for page in range(1, 500):
+        print('huidige pagina: ' + str(page))
         url = f'https://www.bol.com/nl/nl/l/rugzakken/20701/?page={page}'
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -32,6 +30,9 @@ with open('Bol_com_reviews2.csv', 'w', newline='', encoding='utf-8') as csvfile:
             image = soup.find('img', {'class': 'js_selected_image'})['src']
             review_tags = soup.find_all('div', {'class': 'review__body'})
 
-            for review_tag in review_tags:	
+            for review_tag in review_tags:
                 review = review_tag.text.strip()
-                writer.writerow({'name': name.replace('\uFFFD', ''), 'review': review.replace('\uFFFD', ''), 'image': image.replace('\uFFFD', '')})
+                try:
+                    writer.writerow({'name': name, 'review': review.replace('\uFFFD', ''), 'image': image.replace('\uFFFD', '')})
+                except UnicodeEncodeError:
+                    print("Oopsie poopsie, I did a little woopsie")
--- Bol/SchraperImg.py
+++ Bol/SchraperImg.py
@@ -7,9 +7,9 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-# # Create a directory to store the downloaded images
-# if not os.path.exists('images'):
-#     os.makedirs('images')
+# Create a directory to store the downloaded images
+if not os.path.exists('images'):
+    os.makedirs('images')
 
 with open('Bol_com_reviews.csv', 'w', newline='', encoding='utf-16') as csvfile:
     fieldnames = ['name', 'price', 'url', 'image']
@@ -26,19 +26,23 @@ with open('Bol_com_reviews.csv', 'w', newline='', encoding='utf-16') as csvfile:
 
         for tag in product_tags:
             name = tag.find('span', {'class': 'truncate'}).text.strip()
-            price = soup.find('meta', {'itemprop': 'price'})['content']
-            image = soup.find('img', {'class': 'js_product_image'})['src']
             url = tag.find('a', {'class': 'hit-area-listpage'})['href']
+            
             visit_url = f'https://www.bol.com{url}'
-            print(name)
-
-            # # Download the image and save it to a file
-            # image_response = requests.get(image_url)
-            # with open(f'images/{name}.jpg', 'wb') as f:
-            #     f.write(image_response.content)
+            response = requests.get(visit_url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            image = soup.find('img', {'class': 'js_selected_image'})['src']
+            EANcode = soup.find_all('dd', {'class': 'specs__value'})[7].text.strip() 
+            
+            # Download the image and save it to a file
+            image_response = requests.get(image)
+            with open(f'images/{EANcode}.jpg', 'wb') as f:
+                f.write(image_response.content)
             # needs a uniqe code without / and spaces
             
+            print (EANcode)
+            
             try:
-                writer.writerow({'name': name, 'price': price, 'url': visit_url, 'image': image})
+                writer.writerow({'name': name, 'url': visit_url, 'image': image})
             except UnicodeEncodeError:
                 print("Oopsie poopsie, I did a little woopsie")

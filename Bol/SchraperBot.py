147daaf6b04776d1ab93263faac325a2d6fee7f3
Kikkerslijm410 <lucasclavel356@gmail.com>
2023-04-12 14:31:43 +0200

    bye bye

--- Bol/SchraperBot.py
+++ Bol/SchraperBot.py
@@ -38,8 +38,8 @@ def scrape_page(page):
                 except UnicodeEncodeError:
                     print("Oopsie poopsie, I did a little woopsie")
 
-if __name__ == '__main__':
-    with open('Bol_com_reviews.csv', 'w', newline='', encoding='utf-16') as csvfile:
-        csvfile.write('\ufeff')  # Add BOM for Excel compatibility
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(scrape_page, range(1, 500))
+# if __name__ == '__main__':
+#     with open('Bol_com_reviews.csv', 'w', newline='', encoding='utf-16') as csvfile:
+#         csvfile.write('\ufeff')  # Add BOM for Excel compatibility
+#     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+#         executor.map(scrape_page, range(1, 500))
